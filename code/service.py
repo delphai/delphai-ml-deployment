72d08695dbdc3de75f2bd9b767cb051cf1c989a6
ahmedmahmo <ahmed.mahmoud@delphai.com>
2020-09-15 15:39:23 +0200

    add workspace action

--- code/service.py
+++
@@ -1,4 +0,0 @@
-import os
-print("::debug:: get Model Name")
-var = os.environ.get("INPUT_MODEL_NAME", default=None)
-print("Modelname: " + var)
---
+++ src/service.py
@@ -0,0 +1,14 @@
+import os
+import json
+
+print("::debug:: GETTING VARIABLES")
+workspace_name = os.environ.get("INPUT_WORKSPACE_NAME", default=None)
+resource_group = os.environ.get("INPUT_RESOURCE_GROUP", default=None)
+
+
+workspace_dict = {
+    'name'           : workspace_name,
+    'resource_group' : resource_group
+}
+with open("workspace.json", "w") as outfile: 
+    json.dump(workspace_dict, outfile) 
