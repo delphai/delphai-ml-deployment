19a2387ebd31e71888af8d8acbc1802c8451892f
ahmed mahmoud <ahmed-delphai@ahmeds-MacBook-Air.local>
2020-10-13 18:20:26 +0200

    fix typos

--- src/deploy.py
+++ src/deploy.py
@@ -43,13 +43,7 @@ def deploy():
         print(f"::debug::Could not cast model version to int: {exception}")
         model_version = None   
 
-    # Define target cloud
-    if rm_endpoint.startswith("https://management.usgovcloudapi.net"):
-        cloud = "AzureUSGovernment"
-    elif rm_endpoint.startswith("https://management.chinacloudapi.cn"):
-        cloud = "AzureChinaCloud"
-    else:
-        cloud = "AzureCloud"
+    cloud = "AzureCloud"
     
     # Authenticate Azure
     try:
@@ -63,8 +57,8 @@ def deploy():
     
     #Load workspace and resource group
     print("::debug::Loading Workspace values")
-    ws_path        = os.environ.get("INPUT_WORKSPACE", default='delphai-common-ml')
-    resource_group = os.environ.get("INPUT_RESOURCE_GROUP", default='tf-ml-workspace')
+    ws_path        = 'delphai-common-ml'
+    resource_group = 'tf-ml-workspace'
     
     #Load Azure workspace
     try:
@@ -90,7 +84,7 @@ def deploy():
     try:
         deployment_target = ComputeTarget(
             workspace=ws,
-            name=os.environ.get("INPUT_COMPUTE_TARGET", default='delphai-common')
+            name='delphai-common'
         )
     except ComputeTargetException:
         deployment_target = None
@@ -98,13 +92,13 @@ def deploy():
         deployment_target = None
 
     # Loading entry and conda file
-    source = os.environ.get("INPUT_SOURCE_DIR", default="src")
+    source = os.environ.get("INPUT_SOURCE_DIR") or 'src'
 
     print("::debug::Loading entry_file & Conda file")
-    entry_file = os.environ.get("INPUT_ENTRY_FILE", default="entry.py")
+    entry_file = os.environ.get("INPUT_ENTRY_FILE") or 'entry.py'
     entry_file_path = os.path.join(source, entry_file)
     
-    conda_file = os.environ.get("INPUT_CONDA_FILE", default="conda.yml")
+    conda_file = os.environ.get("INPUT_CONDA_FILE") or 'conda.yml'
     conda_ffile_path = os.path.join(source, conda_file)
     
     inference_configration = InferenceConfig(
@@ -116,13 +110,13 @@ def deploy():
         #print('::debug:: Make sure conda.yml and entry.py are in the [src] directory')
     
     print('::debug:: get namespace and replicas')
-    replicas = os.environ.get('INPUT_REPLICAS',default=3)
+    replicas = os.environ.get('INPUT_REPLICAS') or '3'
     try:
         replicas = int(replicas)
     except TypeError as exception:
         print(f"::debug::Could not cast model version to int: {exception}")
         replicas = 3   
-    namespace= os.environ.get('INPUT_NAMESPACE')
+        
     deployment_name = os.environ.get('INPUT_DEPLOYMENT_NAME',default=model_name.replace("_","-"))
     create_namespace(app_id=app_id, app_secret=app_secret, tenant=tenant_id,namespace=deployment_name)
     deployment_configration= AksWebservice.deploy_configuration(autoscale_enabled=False, num_replicas=replicas,namespace=deployment_name)
